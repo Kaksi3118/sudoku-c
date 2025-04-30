8ce60eea845a78a74d9c2b8408d9686c6ad85df8
Kaksi3118 <maksymilian.jura3118@gmail.com>
2025-04-30 13:14:07 +0200

    Changed sudoku.h to only create 9x9 boards

--- sudoku.h
+++ sudoku.h
@@ -1,20 +1,10 @@
 #ifndef SUDOKU_H
 #define SUDOKU_H
 
-#include <stdio.h>
-#include <stdlib.h>
-// Create NxN board
-int **create_grid(int N);
-// Free the grid
-void free_grid(int **grid, int N);
+// Generate a full 9×9 solution and remove (81 - hints) cells,
+void generateSudoku(int grid[9][9], int hints);
 
-void generate_sudoku(int **grid, int N, int B, int k);
-
-// Print the Sudoku board
-void print_grid(int **grid, int N);
-
-int save_game(const char *filename, int **grid, int N, int k_remaining);
-
-int load_game(const char *filename, int ***grid_out, int *N_out, int *k_remaining_out);
+// Print the 9×9 grid
+void printGrid(int grid[9][9]);
 
 #endif
