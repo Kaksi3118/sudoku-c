9e2d83fa0b14a234e9beaab364c7f957e89f4cd0
Kaksi3118 <maksymilian.jura3118@gmail.com>
2025-04-30 14:59:29 +0200

    Fixed playGame() so newGame can call it, added isCorrectMove() to sudoku.h

--- sudoku.h
+++ sudoku.h
@@ -4,6 +4,9 @@
 // Generate a full 9×9 solution and remove (81 - hints) cells,
 void generateSudoku(int puzzle[9][9], int solution[9][9], int hints);
 
+// Check if a move is correct
+int isCorrectMove(int solution[9][9], int row, int col, int val);
+
 // Print the 9×9 grid
 void printGrid(int grid[9][9]);
 
